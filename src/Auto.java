36143aa3ad78642ef315005fb78a01a8eee6e893
Btai1808 <148732160+Btai1808@users.noreply.github.com>
2023-11-26 13:25:39 -0500

    Creación de clases

--- src/Auto.java
+++ src/Auto.java
@@ -1,2 +1,22 @@
-public class Auto {
+// Auto.java
+public class Auto implements ImpactoEcologico {
+    private String modelo;
+    private double emisiones;
+
+    public Auto(String modelo, double emisiones) {
+        this.modelo = modelo;
+        this.emisiones = emisiones;
+    }
+
+    @Override
+    public double obtenerImpactoEcologico() {
+        // Implementa el cálculo del impacto ecológico para un auto
+        // Puedes usar información específica sobre las emisiones, eficiencia de combustible, etc.
+        return emisiones * 0.2; // Ejemplo simplificado, ajusta según tus necesidades
+    }
+
+    @Override
+    public String toString() {
+        return "Auto: " + modelo;
+    }
 }
